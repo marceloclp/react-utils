11dd8f533152521b84db238b9be33f45e1f5031a
Marcelo Perrella <marceloclperrella@gmail.com>
2022-05-26 19:20:54 +0100

    build: attempt to bump latest commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from './types'
+export * from './utils/shorthands'
 
 export { filterProps } from './utils/filter-props'
 export { getSyncRefsCallback } from './utils/get-sync-refs-callback'
 export { mergeProps } from './utils/merge-props'
 export { renderAs } from './utils/render-as'
-export * from './utils/shorthands'
